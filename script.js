82e89cb4c5acd2d046ad4f4f9fd1d357fbadee4c
Alixia <lizziejones1208@gmail.com>
2022-10-14 00:09:14 +0800

    Fix variable storing for calculations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a,b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a,b) {
@@ -14,38 +14,69 @@ function divide(a,b) {
     return a/b;
 }
 
-function operate(a,b) {
-    return add(a,b);
-    /* Get two numbers a and b
-    run a and b through an operator
-    */
+function clear() {
+    a = '';
+    b = '';
+    operator = '';
+    firstOperand.innerHTML = '';
 }
 
 
+function calculate() {
+    if(operator == '+') {
+        secOperand.innerHTML = '';
+        firstOperand.innerHTML = add(a,b);
+    } else {
+        firstOperand.innerHTML = 'Error';
+    }
+}
 
-const firstOperand = document.getElementById('prev-operand');
+
+const firstOperand = document.getElementById('first-operand');
 const secOperand = document.getElementById('next-operand');
 const numBtns = document.querySelectorAll('.calc-numbers');
 const operators = document.querySelectorAll('.calc-operator');
-const clear = document.querySelector('.calc-clear');
+const clearBtn = document.querySelector('.calc-clear');
 const del = document.querySelector('.calc-del');
+const eq = document.querySelector('.calc-equals');
 
-clear.addEventListener('click', () => {
-    firstOperand.innerHTML = '';
-    secOperand.innerHTML = '';
+let a = '';
+let b = '';
+let operator = '';
+
+/*Store numbers in display to a variable
+Pick an operator, store in a variable. and clear display
+Store second number to another variable
+Equals btn must clear display, apply operator on first and second, and display result
+*/
+
+
+clearBtn.addEventListener('click', () => {
+    clear()
+});
+
+eq.addEventListener('click', () => {
+    calculate();
 })
 
 
-operators.forEach(operator => {
-    operator.addEventListener('click', () => {
-            firstOperand.innerHTML += operator.innerHTML;
-        
+operators.forEach(e => {
+    e.addEventListener('click', () => {
+        firstOperand.innerHTML = '';
+        return operator = e.innerHTML;
     })
 })
 
 
 numBtns.forEach(numBtn => {
     numBtn.addEventListener('click', () => {
-        firstOperand.innerHTML += numBtn.innerHTML;
+    if(!operator) { 
+        a += numBtn.innerHTML;
+        firstOperand.innerHTML = a;
+    } else {
+        b += numBtn.innerHTML;
+        secOperand.innerHTML = b;
+    }
     })
 })
+
